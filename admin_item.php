441846e0d1042713da75522d5f186295a6fa02ee
jethwa-keyuri <keyurijethava@gmail.com>
2023-02-26 15:57:05 +0530

    Update admin_item.php

--- admin_item.php
+++ admin_item.php
@@ -27,8 +27,8 @@ background-repeat: no-repeat; background-size: cover;
  
   <div class="container text-white ">
     
-        <h1 class="text-center text-warning mt-5 p-4">Items</h1>
-        <a href=""><button class="text-white bg-warning rounded fw-bold">+ New Items</button></a>
+        <h1 class="text-center text-warning mt-5 p-4 fst-italic">Items</h1>
+        <a href="admin_new_item.php"><button class="text-white bg-warning rounded fw-bold">+ New Items</button></a>
         <div class="table-responsive">
             <table class="table text-white my-sm-5">
                 <thead>
