d34c864cdc6c1745945d8f8ae02be9c7c3ef865a
jethwa-keyuri <keyurijethava@gmail.com>
2023-03-20 20:56:08 +0530

    Update pizza.php

--- pizza.php
+++ pizza.php
@@ -15,54 +15,55 @@ background-position: center;
 background-repeat: no-repeat; background-size: cover;
 ">
   <?php
-  include_once('nav.php')
+  include_once('login_nav.php')
   ?>
   <div class="container mt-5">
     <div class="row mx-5 p-4">
       <div class="col-md-6 col-12">
-        <img src="img/pizza-1.jpg" class="img-fluid">
+        <img src="img/pizza-1.jpg" class="img-fluid  shadow p-1 mb-5 bg-dark rounded accordion ">
       </div>
       <div class="col-md-6 col-12 text-white">
         <div class="row float-start">
           <h2 class="text-warning">Italian pizza</h2>
           <p>This pizza is very delicious.A hugely popular margherita, with a deliciously tangy single cheese topping</p>
-          <p> Price: $20</p>
-          <p> on Dicount 50% use the coupon code</p>
-          <p> Size of pizza:</p>
-          <p>
-            <input type="radio" class="">Small
-            <input type="radio" class="">Medium
-            <input type="radio" class="">Large
-          </p>
-          <p>
+          <p class="text-warning fs-5 m-2"> Price</p><p class="mx-2">$20</p> 
+          <p class="mx-2 fs-4"> On Dicount 50% use the coupon code</p>
+          <p class="text-warning fs-5 m-2 mb-3"> Size of pizza:</p>
+          <Div class="d-flex">
+          <input type="radio" class="m-2">Small
+          <input type="radio" class="m-2">Medium
+          <input type="radio" class="m-2">Larger
+          </Div>
+          <br>
+          <p class="text-warning m-2 fs-5">
             Select Extra Ingredients:
           </p>
           
          <div class="d-flex p-2">
-            <div class="mx-2">Cheese:</div>
+            <div class="m-2 ">Cheese:</div>
             <div class=" d-flex">
               
-              <div><input type="button" value="-"></div>
+              <div><input type="button" value="-" class="m-2"></div>
               <div class="mx-2"> 1 </div>
-              <div><input type="button" value="+"></div>
+              <div><input type="button" value="+" class="m-2"></div>
             </div>
           </div><br>
           <div class="d-flex p-2">
-            <div class="mx-2">Tamato:</div>
+            <div class="m-2">Tamato:</div>
             <div class=" d-flex">
               
-              <div><input type="button" value="-"></div>
+              <div><input type="button" value="-"  class="m-2"></div>
               <div class="mx-2"> 1 </div>
-              <div><input type="button" value="+"></div>
+              <div><input type="button" value="+"  class="m-2"></div>
             </div>
           </div><br>
           <div class="d-flex p-2">
-            <div class="mx-2">Onion:</div>
+            <div class="m-2"> Onion: </div>
             <div class="d-flex">
               
-              <div><input type="button" value="-"></div>
+              <div><input type="button" value="-"  class="m-2"></div>
               <div class="mx-2"> 1 </div>
-              <div><input type="button" value="+"></div>
+              <div><input type="button" value="+"  class="m-2"></div>
             </div>
           </div>
           <a href="cart.php"><input type="button" value="Add Cart" class="btn btn-warning w-100"></a>
