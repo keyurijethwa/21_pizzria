dcb74a62c49879a2e0a44cd8ed06a9cc22244296
jethwa-keyuri <keyurijethava@gmail.com>
2023-04-10 20:51:52 +0530

    Create remove_menu.php

---
+++ remove_menu.php
@@ -0,0 +1,19 @@
+<?php
+include_once('admin_nav.php');
+$id = $_REQUEST['id'];
+$q = "update menu set Status='Delete' where menu_id='$id'";
+if (mysqli_query($con, $q)) {
+    ?>
+    <script>
+        alert("User Deleted Successfully");
+    </script>
+    <?php
+} else {
+    ?>
+    <script>alert("User Deletion Failed");</script>
+    <?php
+}
+?>
+<script>
+    window.location = "admin_menu.php";
+</script>
