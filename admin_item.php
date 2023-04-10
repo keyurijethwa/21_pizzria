aa6d21cd4a878097ba108a496edf3c1fe39557a7
jethwa-keyuri <keyurijethava@gmail.com>
2023-04-10 21:41:46 +0530

    Update admin_item.php

--- admin_item.php
+++ admin_item.php
@@ -1,4 +1,12 @@
-
+<?php
+include_once('database.php');
+$q = "select * from item";
+$result = mysqli_query($con, $q);
+?>
+<?php
+session_start();
+if (isset($_SESSION['emailid']) && isset($_SESSION['password'])) {
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -10,7 +18,7 @@
   <link rel="stylesheet" href="css/bootstrap.css">
   <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="font/css/all.css">
-  
+
 </head>
 
 <body style="
@@ -20,47 +28,71 @@ background-position: center;
 background-repeat: no-repeat; background-size: cover;
 ">
   <?php
-    include_once('admin_nav.php')
+  include_once('admin_nav.php')
   ?>
 
   <!-- menu  -->
- 
+
   <div class="container text-white ">
-    
-        <h1 class="text-center text-warning mt-5 p-4 fst-italic">Items</h1>
-        <a href="admin_new_item.php"><button class="text-white bg-warning rounded fw-bold">+ New Items</button></a>
-        <div class="table-responsive">
-            <table class="table text-white my-sm-5">
-                <thead>
-                    <tr style="background-color: #ffc107;" class="text-center">
-                    <th scope="col">Menu_id</th>
-                        <th scope="col">Item_name</th>
-                        <th scope="col">Item_id</th>
-                        <th scope="col">Price</th>
-                        <th scope="col">Description</th>
-                        <th scope="col">Ingredients</th>
-                        <th scope="col">item_pic</th>
-                        <th scope="col">Edit</th>
-                        <th scope="col">Remove</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr class="text-center">
-                        <td>1120001</td>
-                        <td>pizza2</td>
-                        <td>4120001</td>
-                        <td>$30</td>
-                        <td>This pizza is so soft made by speacial chef</td>
-                        <td>with Tomato,mozrella</td>
-                        <td><img src="img/i-1.jpeg" alt="" height="50%" width="50%"></td>
-                        <td><a href="edit_item.php"><i class="fa-regular fa-pen-to-square"></i></a></td>
-                        <td scope="row"><i class="fa-solid fa-trash"></i></td>
-                    </tr>
-                    
-                </tbody>
-            </table>
-        </div>
+
+    <h1 class="text-center text-warning mt-5 p-4 fst-italic">Items</h1>
+    <a href="admin_new_item.php"><button class="text-white bg-warning rounded fw-bold">+ New Items</button></a>
+    <div class="table-responsive">
+      <table class="table text-white my-sm-5">
+        <thead>
+          <tr style="background-color: #ffc107;" class="text-center">
+            <th scope="col">Menu_id</th>
+            <th scope="col">Item_name</th>
+            <th scope="col">Item_id</th>
+            <th scope="col">Price</th>
+            <th scope="col">Description</th>
+            <th scope="col">Ingredients</th>
+            <th scope="col">item_pic</th>
+            <th scope="col">Status</th>
+            <th scope="col">Edit</th>
+            <th scope="col">Remove</th>
+            <th scope="col">Activative</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php
+          while ($b = mysqli_fetch_array($result)) {
+          ?>
+             <tr>
+              <td><?php echo $b[7]?></td>
+              <td><?php echo $b[0]?></td>
+              <td><?php echo $b[1]?></td>
+              <td><?php echo $b[2]?></td>
+              <td><?php echo $b[3]?></td>
+              <td><?php echo $b[4]?></td>
+              <td><img src="item_image/<?php echo $b[5]?>" alt="" height="50%" width="50%"></td>
+              <td><?php echo $b[6]?></td>
+              <td><a href="edit_item.php?id=<?php echo $b[1]; ?>"><i class="fa-regular fa-pen-to-square"></i></a></td>
+              <td scope="row"><a href="remove_item.php?id=<?php echo $b[1]; ?>"><i class="fa-solid fa-trash"></i></a></td>
+              <?php if ($b[6] == "Active") {?>
+                <td> <a href="admin_deactivate_item.php?id=<?php echo $b[1]; ?>"><button class="btn btn-warning" style="width:100px">Deactivate</button></a>
+                </td>
+            <?php
+            } else if ($b[6] == "Not Active") {
+            ?>
+                <td> <a href="admin_activate_item.php?id=<?php echo $b[1]; ?>"><button class="btn btn-warning" style="width:100px">Activate</button></a>
+                </td>
+            <?php
+            } else if($b[6]=="Delete") {
+            ?>
+                <td> <a href="admin_reactivate_deleted_item.php?id=<?php echo $b[1]; ?>"><button class="btn btn-warning" style="width:100px">Reactivate</button></a>
+                </td>
+            <?php
+            }
+            ?>
+            </tr>
+          <?php
+          }
+          ?>
+        </tbody>
+      </table>
     </div>
+  </div>
 
   <!-- menu end -->
 
@@ -70,3 +102,12 @@ background-repeat: no-repeat; background-size: cover;
 </body>
 
 </html>
+<?php
+} else {
+?>
+    <script>
+        window.location = "login.php";
+    </script>
+<?php
+}
+?>
